b1f54ebcc990c0df03cf262c9b40cbf462a28741
panpan <921513729@qq.com>
2022-07-20 20:03:18 +0800

    增加单点配置

---
+++ apispring/apisecurity/sso/server/src/main/java/com/OAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author pan
+ * @Date 2022/7/20 8:33
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableResourceServer
+public class OAuthServerApplication {
+}
